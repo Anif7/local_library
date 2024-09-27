158a99e8dd67155fcc3b6b9b5b121e6c934943cf
Anif <anif@testpress.in>
2024-09-27 11:41:53 +0530

    Completed creating Models, Views, Templates and Unit Tests Ready to Production

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+from django.conf import settings
+from datetime import date 
 import uuid
 
 class MyModelName(models.Model):
@@ -75,6 +77,7 @@ class Book(models.Model):
     
 
 
+
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
     id=models.UUIDField(primary_key=True,default=uuid.uuid4,
@@ -82,6 +85,13 @@ class BookInstance(models.Model):
     book=models.ForeignKey('Book',on_delete=models.RESTRICT,null=True)
     imprint=models.CharField(max_length=200)
     due_back=models.DateField(null=True,blank=True)
+    borrower=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,null=True,blank=True)
+    
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
+
 
     LOAN_STATUS=(
         ('m', 'Maintenance'),
@@ -100,6 +110,7 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering=['due_back']
+        permissions=(('can_mark_returned','Set book as returned'),('can_view_borrowers_page',"view the all the borrowers page"))
 
     def __str__(self):
         """String for representing the Model object."""
